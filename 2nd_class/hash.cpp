6302520282171b68fec55f5fbf8d92fc3e8935b4
wawawa113 <ryo241334@icloud.com>
2022-10-14 15:11:45 +0900

    add 2nd class submission

--- 2nd_class/hash.cpp
+++ 2nd_class/hash.cpp
@@ -12,7 +12,7 @@ class Hash{
         M=M_;
         Delta=Delta_;
         done=vector<int>(M);
-        v=vector<int>(M);
+        v=vector<int>(M,-1);
     }
     void Insert(int k){
         int now=k%M;
@@ -31,6 +31,23 @@ class Hash{
     void print(){
         cout<<"v : ";
         for(auto i:v)cout<<i<<" ";cout<<endl;
+    }
+    bool search(int k){
+        int now=k%M;
+        int step=0;
+        while(v[now]!=k){
+            if(step>M){
+                cout<<"CAN'T FOUND"<<endl;return false;
+            }
+            if(done[now]==false){
+                cout<<"CAN'T FOUND"<<endl;return false;
+            }
+            step++;
+            now+=Delta;
+        }
+        cout<<"FOUNDED"<<endl;
+        cout<<"step : "<<step<<endl;
+        return true;
     }
 };
 
@@ -55,4 +72,7 @@ int main()
     h.print();
     h.Insert(3);
     h.print();
+
+    cout<<h.search(1)<<endl;//ある
+    cout<<h.search(1000)<<endl; //ない
 }
